bed58d34c0e341843078be471bbfff214d2d0ffc
KeertiParawati <39194861+KeertiParawati@users.noreply.github.com>
2026-03-09 16:40:11 +0530

    Add files via upload

---
+++ Conditional.py
@@ -0,0 +1,12 @@
+name=input("please enter your name")
+print ("hello"+name)
+num=int (input("please enter your number"))
+print (num)
+if num >0:
+ print  (" it is postive")  
+elif num <0:
+ print("it is nataive")
+else:
+ print("num is zoro")
+
+
