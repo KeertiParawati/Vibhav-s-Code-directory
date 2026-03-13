286ce46b2decf6ac68b3bb2357781a70555d3547
KeertiParawati <39194861+KeertiParawati@users.noreply.github.com>
2026-03-13 16:06:29 +0530

    Add files via upload

---
+++ Data types in python.py
@@ -0,0 +1,10 @@
+a = 34
+print("data type of a is",type(a))
+b = False
+print("data type of b is",type(b))
+c = 4.55
+print("data type of c is",type(c))
+d = "Vibhav"
+print("Data type of d is",type(d))
+word = input("Enter any sting")
+print(word[::-1])
