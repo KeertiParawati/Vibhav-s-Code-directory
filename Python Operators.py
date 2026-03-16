63ad409a4842cb81232f50bd026f95ac1c8aedf6
KeertiParawati <39194861+KeertiParawati@users.noreply.github.com>
2026-03-16 15:28:52 +0530

    Add files via upload

---
+++ Python Operators.py
@@ -0,0 +1,9 @@
+number1=int(input("please enter the first number"))
+number2=int (input("please enter the second number"))
+print ("add",number1 + number2)
+
+print ("sub",number1 - number2)
+
+print ("mul",number1 * number2)
+
+print ("div",number1 / number2)
