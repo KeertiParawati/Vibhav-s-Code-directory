fde022fc5ec13fa4479b83c0a1ded4629f9b9d86
KeertiParawati <39194861+KeertiParawati@users.noreply.github.com>
2026-03-20 10:17:23 +0530

    Add files via upload

---
+++ o.py
@@ -0,0 +1,25 @@
+a=10
+b=10
+if a and b ==10:
+    print ("both are same")
+else:
+    print ("both are different")
+
+c=10
+d=13
+if c and d ==10:
+    print ("both are same")
+else:
+    print ("both are different")    
+
+e=14
+f=13
+if e or f < 10:
+    print ("one value is same")
+else:
+    print ("both value is different")   
+
+g="aadyant"     
+h="vibhav"
+if g !=h:
+    print ("both are different")
