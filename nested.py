ccfb0748a30f0ffe623e8dcc645fa3fef09923aa
KeertiParawati <39194861+KeertiParawati@users.noreply.github.com>
2026-03-24 10:35:30 +0530

    Add files via upload

---
+++ nested.py
@@ -0,0 +1,25 @@
+print("1.CAR")
+print("2.BIKE")
+print("3.BUS")
+print("4.TRAIN")
+option = int (input("please select your option")) 
+if option == 1:
+    print ("you have seltect car")
+    print("What is your brand ?")
+    print("1. tata")
+    print("2. xev9e")
+
+    brand = input("enter your brand number: ")
+
+    if brand == "1":
+
+     print("you have selected tata")
+    elif brand == "2":
+
+     print("you have selected xev9e")
+elif option == 2:
+    print ("you have seltect bike")
+elif option == 3:
+    print ("you have seltect bus")
+elif option == 4:
+    print ("you have seltect train")
