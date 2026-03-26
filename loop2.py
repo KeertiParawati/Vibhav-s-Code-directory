8c29fa4d71cd5c32e173c4ace95fa1834296ccc2
KeertiParawati <39194861+KeertiParawati@users.noreply.github.com>
2026-03-26 15:11:09 +0530

    Add files via upload

---
+++ loop2.py
@@ -0,0 +1,28 @@
+for i in range  (1,50):
+    print (i)
+
+
+num = 10
+for i in range (1,11):
+    mul = num *i                               
+    print (num,"x",i,"=",mul) 
+
+a = 20 
+for i in range (20,0,-1):
+    print (i)
+
+
+string = input("Please enter your own String : ")
+
+string2 = ('')
+
+
+
+for i in string:
+
+ string2 = i + string2
+
+
+print("\nThe Original String = ", string)
+
+print("The Reversed String = ", string2)    
