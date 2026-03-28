9c923a8a1e15c1ca04bf62e9cc7f7e0c88b51a02
KeertiParawati <39194861+KeertiParawati@users.noreply.github.com>
2026-03-28 10:56:22 +0530

    Add files via upload

--- loop.py
+++ loop.py
@@ -5,4 +5,18 @@ for i in range(1,30):
 num=35   
 for i in range(1,11):
  mul=num *i
- print(num, "x", i, "=", mul)
+ print(num, "x", i, "=", mul)  
+
+num = int (input("please enter your number"))
+sum = 0
+i = 1 
+while i <= num :
+   sum = sum + i
+   i = i + 1 
+print (sum)   
+
+for i in range (500,601) :
+   print (i) 
+i = 0
+while i <= 0:
+   print ("i will run forever")
