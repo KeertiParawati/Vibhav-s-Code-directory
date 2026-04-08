0fdfc88f603b9861f32d19440f79e427fa0d5ea9
KeertiParawati <39194861+KeertiParawati@users.noreply.github.com>
2026-04-08 14:45:16 +0530

    Add files via upload

---
+++ Functions2.py
@@ -0,0 +1,23 @@
+def well_wishes():
+    print("hi how are you")
+    print ("cool as usal")
+well_wishes() 
+
+
+def add(x,y):
+    return x + y 
+print(add(2,3))
+
+def sub(x,y):
+    return x - y
+print(sub(2,3))
+
+
+def mul(x,y):
+    return x * y
+print (mul(2,3))
+
+
+def div(x,y):
+    return x / y 
+print(div(2,3)) 
