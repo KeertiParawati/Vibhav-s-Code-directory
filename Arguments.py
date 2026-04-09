8b63bd517248714c6befaeaa94c9d9911e392938
KeertiParawati <39194861+KeertiParawati@users.noreply.github.com>
2026-04-09 14:41:14 +0530

    Add files via upload

---
+++ Arguments.py
@@ -0,0 +1,28 @@
+def add(x,y,z):
+    return x + y + z
+ans = add(12,23,14)
+print(ans)
+
+def sub(x,y,z):
+    return x - y - z
+ans = sub(34,95,78)
+print(ans)
+
+def mul(x,y,z):
+    return x * y * z
+ans = mul(67,89,85)
+print(ans)
+
+def div(x,y):
+    return x / y 
+ans = div(27,23)
+print(ans)
+
+
+def cube(a):
+    return a * a * a
+print(cube(32))       
+
+def square(a):
+    return a * a 
+print(square(32))      
