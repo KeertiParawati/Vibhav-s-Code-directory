ebb2043297139404171a42c861ee080810dfe54a
KeertiParawati <39194861+KeertiParawati@users.noreply.github.com>
2026-05-04 10:11:01 +0530

    Add files via upload

---
+++ Tuples.py
@@ -0,0 +1,17 @@
+Tuples = ("tuple",False,3,2,1)
+
+print(Tuples)
+
+Tuples = (4,6,2,8,3,1)
+
+print(Tuples)
+
+Tuple1 = (50,10,60,70,50)
+
+print(Tuple1.count(50))
+
+Tuples = (2,4,3,5,4,6,7,8,6,1)
+
+slice = Tuples[3:5]
+
+print(slice)
