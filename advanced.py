889b285a80ae0e5be297f3ecfb2b17232872ae2b
KeertiParawati <39194861+KeertiParawati@users.noreply.github.com>
2026-05-14 13:52:23 +0530

    Add files via upload

---
+++ advanced.py
@@ -0,0 +1,32 @@
+
+# Add two lists using map and lambda
+
+numbers1 = [1, 2, 3]
+
+numbers2 = [4, 5, 6]
+
+result = map(lambda x, y: x + y, numbers1, numbers2)
+
+print("Addition of two lists")
+
+print(list(result))
+
+nums = [1,2,3,4,5]
+
+def sq(n):
+
+    return n*n
+
+square = ("square of numbers in list")
+
+print(square) 
+
+for i in range (10):
+
+    if i == 5:
+
+        print(exit)
+
+        exit()
+
+    print(i)
