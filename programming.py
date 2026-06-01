34b9a6fc694f28accd0994ec26331d10ee2e450b
KeertiParawati <39194861+KeertiParawati@users.noreply.github.com>
2026-06-01 16:36:42 +0530

    Add files via upload

---
+++ programming.py
@@ -0,0 +1,21 @@
+# create a class
+
+class student:
+
+ grade = 10
+
+ print("Hi I am a student of grade",grade)
+
+# create an object
+
+ob = student()
+
+class student:
+ 
+ grade = 10
+
+ print("Hi I am a student of grade",student)
+
+ # create an object
+
+ob = student()
